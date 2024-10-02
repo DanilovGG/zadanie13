235cac88b3f1f56afb30515aa76565a98428a041
DanilovGG <derzhik95@gmail.com>
2024-10-02 19:16:45 +0300

    zadanie13-p2

--- src/test/java/ShopRepositoryTest.java
+++ src/test/java/ShopRepositoryTest.java
@@ -6,26 +6,26 @@ class ShopRepositoryTest {
     @Test
     public void testRemoveProduct() {
         ShopRepository repo = new ShopRepository();
-        Product product1 = new Product(1, "Яблоко", 100);
-        Product product2 = new Product(2, "Банан", 200);
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(2, "Банан", 200);
 
-        repo.add(product1);
-        repo.add(product2);
+        repo.add(p1);
+        repo.add(p2);
         repo.remove(2);
 
         Product[] action = repo.findAll();
-        Product[] expected = {product1};
+        Product[] expected = {p1};
         assertArrayEquals(expected, action);
     }
 
     @Test
     public void testRemoveNoProduct() {
         ShopRepository repo = new ShopRepository();
-        Product product1 = new Product(1, "Яблоко", 100);
-        Product product2 = new Product(2, "Банан", 200);
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(2, "Банан", 200);
 
-        repo.add(product1);
-        repo.add(product2);
+        repo.add(p1);
+        repo.add(p2);
 
         assertThrows(
                 NotFoundException.class,
@@ -35,9 +35,77 @@ class ShopRepositoryTest {
 
     @Test
     void testHashCodeUniqueness() {
-        Product apple = new Product(1, "Яблоко", 100);
-        Product banana = new Product(2, "Банан", 200);
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(2, "Банан", 200);
 
-        assertNotEquals(apple.hashCode(), banana.hashCode());
+        assertNotEquals(p1.hashCode(), p2.hashCode());
+    }
+
+    @Test
+    void testEquals_shouldReturnTrueForSameInstances() {
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(1, "Яблоко", 100);
+
+        boolean result = p1.equals(p2);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testEquals_shouldReturnFalseForNull() {
+        Product p1 = new Product(1, "Яблоко", 100);
+
+        boolean result = p1.equals(null);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testEquals_shouldReturnTrueForIdenticalThis() {
+        Product p1 = new Product(1, "Яблоко", 100);
+
+        boolean result = p1.equals(p1);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testEquals_shouldReturnFalseForDifferentClasses() {
+        Product p1 = new Product(1, "Яблоко", 100);
+        Object o = new Object();
+
+        boolean result = p1.equals(o);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testEquals_shouldReturnFalseForDifferentIds() {
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(2, "Яблоко", 99);
+
+        boolean result = p1.equals(p2);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testEquals_shouldReturnFalseForDifferentTitles() {
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(1, "Банан", 100);
+
+        boolean result = p1.equals(p2);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testEquals_shouldReturnFalseForDifferentPrices() {
+        Product p1 = new Product(1, "Яблоко", 100);
+        Product p2 = new Product(1, "Яблоко", 99);
+
+        boolean result = p1.equals(p2);
+
+        assertFalse(result);
     }
 }
